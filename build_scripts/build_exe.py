1ae8a1de3ab4c97ac857e449652a6886241f8216
cyqmq <3532334911@qq.com>
2026-06-30 17:44:13 +0800

    ⬆️ v1.0.5

--- build_scripts/build_exe.py
+++ build_scripts/build_exe.py
@@ -17,7 +17,7 @@ import subprocess
 ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 os.chdir(ROOT)
 
-VERSION = "1.0.4"
+VERSION = "1.0.5"
 TOOLS_DIR = os.path.join(ROOT, "tools")
 DIST_DIR = os.path.join(ROOT, "dist")
 SPEC_DIR = os.path.join(ROOT, "build_scripts")
